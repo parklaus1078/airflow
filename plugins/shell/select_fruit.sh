a794536e4b8cbe0f59a80d628a2feffb0abe3007
Geun Woo Park <parklaus1078@gmail.com>
2023-12-13 13:38:58 +0900

    Shell script added

---
+++ plugins/shell/select_fruit.sh
@@ -0,0 +1,10 @@
+FRUIT=$1
+if [$FRUIT == APPLE];then
+    echo "You selected Apple!"
+if [$FRUIT == ORANGE];then
+    echo "You selected Orange!"
+if [$FRUIT == Grape];then
+    echo "You selected Grape!"
+else 
+    echo "You selected something else!"
+fi
