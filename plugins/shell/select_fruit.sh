e5b28b445d44f484a3fdf48e2b6de81cc15cb3b4
Geun Woo Park <parklaus1078@gmail.com>
2023-12-13 13:49:39 +0900

    fixed shell file

--- plugins/shell/select_fruit.sh
+++ plugins/shell/select_fruit.sh
@@ -1,10 +1,10 @@
 FRUIT=$1
-if [$FRUIT == APPLE];then
-    echo "You selected Apple!"
-if [$FRUIT == ORANGE];then
-    echo "You selected Orange!"
-if [$FRUIT == Grape];then
-    echo "You selected Grape!"
+if [ $FRUIT == APPLE ];then
+        echo "You selected Apple!"
+elif [ $FRUIT == ORANGE ];then
+        echo "You selected Orange!"
+elif [ $FRUIT == Grape ];then
+        echo "You selected Grape!"
 else 
-    echo "You selected something else!"
+        echo "You selected something else!"
 fi
