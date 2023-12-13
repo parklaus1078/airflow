c994a769b783925c281f269997db05cf57214e58
Geun Woo Park <parklaus1078@gmail.com>
2023-12-13 19:47:47 +0900

    dag_id edited

--- dags/dags_python_task_decorator.py
+++ dags/dags_python_task_decorator.py
@@ -3,7 +3,7 @@ import pendulum
 from airflow.decorators import task
 
 with DAG(
-    dag_id="example_python_operator",
+    dag_id="dags_python_task_decorator",
     schedule="0 2 * * 1",
     start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
     catchup=False,
